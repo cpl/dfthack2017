5d7cafe8e40a8654092c2cfb422f80a0c4eee0db
Alexandru-Paul Copil <copil.alexander@gmail.com>
2017-03-26 03:53:23 +0100

    New CSV processing

--- api.py
+++ api.py
@@ -1,20 +1,26 @@
+"""DOCSTRING."""
+
 import json
 import csv
 import requests
 
 
-response = requests.get('http://rtl2.ods-live.co.uk/api/vehiclePositionHistory?key=wyFhgvh701&date=2017-03-24&vehicle=&from=00:00:00&to=23:59:59').content
-response = json.loads(response)
-
 data = []
-for resp in response:
-    data.append({'date': resp['observed'], 'lat': resp['latitude'], 'lon': resp['longitude']})
+for hour in range(0, 24, 1):
+    print 'HOUR ', hour
+    for minute in range(0, 6010):
 
-print len(data)
+        magic = 'http://rtl2.ods-live.co.uk/api/vehiclePositionHistory?key=wyFhgvh701&date=2017-03-24&vehicle=&from={:02d}:{:02d}:00&to={:02d}:{:02d}:59'.format(hour, minute, hour, minute+9)
+
+        response = requests.get(magic).content
+        response = json.loads(response)
+
+        for resp in response:
+            data.append({'timestamp': resp['observed'], 'latitude': resp['latitude'], 'longitude': resp['longitude'], 'vehicle': resp['vehicle']})
 
 f = csv.writer(open("test.csv", "wb+"))
 
-f.writerow(["date", "lat", "lon"])
+f.writerow(["timestamp", "vehicle", "latitude", "longitude"])
 
 for d in data:
-    f.writerow([d['date'], d['lat'], d['lon']])
+    f.writerow([d['timestamp'], d['vehicle'], d['latitude'], d['longitude']])
